be56de0f8a715838b33bcd02049f2976f48bed10
[dyad] <git@dyad.sh>
2025-09-02 21:59:02 +0530

    [dyad] Restore calculator tabs and add animations - wrote 2 file(s)

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -6,23 +6,23 @@ import FormulaLibrary from "@/components/FormulaLibrary";
 
 const CalculatorContainer = () => {
   return (
-    <Tabs defaultValue="normal" className="w-full max-w-md mx-auto animate-pop-in">
+    <Tabs defaultValue="normal" className="w-full max-w-md mx-auto">
       <TabsList className="grid w-full grid-cols-4">
         <TabsTrigger value="normal">Normal</TabsTrigger>
         <TabsTrigger value="scientific">Scientific</TabsTrigger>
         <TabsTrigger value="marks">Internal Marks</TabsTrigger>
         <TabsTrigger value="formulas">Formulas</TabsTrigger>
       </TabsList>
-      <TabsContent value="normal" className="mt-4">
+      <TabsContent value="normal" className="mt-4 animate-pop-in">
         <Calculator />
       </TabsContent>
-      <TabsContent value="scientific" className="mt-4">
+      <TabsContent value="scientific" className="mt-4 animate-pop-in">
         <ScientificCalculator />
       </TabsContent>
-      <TabsContent value="marks" className="mt-4">
+      <TabsContent value="marks" className="mt-4 animate-pop-in">
         <InternalMarkCalculator />
       </TabsContent>
-      <TabsContent value="formulas" className="mt-4">
+      <TabsContent value="formulas" className="mt-4 animate-pop-in">
         <FormulaLibrary />
       </TabsContent>
     </Tabs>
